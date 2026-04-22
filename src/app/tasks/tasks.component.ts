816428dc4a3e9225b6f00b506d19e5c533dc2108
Norman <normanramadhan3@gmail.com>
2026-04-23 05:43:27 +0700

    add new comp

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,20 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 import { DUMMY_TASK } from '../dummy-task';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
   tasks = DUMMY_TASK;
 
   get selectedUserTasks() {
     return this.tasks.filter(task => task.userId === this.userId);
+  }
+
+  onCompleteTask(taskId: string) {
+    console.log(`Task with ID ${taskId} completed!`);
+    // Di sini Anda bisa menambahkan logika untuk menghapus task dari daftar atau menandainya sebagai selesai.
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  onAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
